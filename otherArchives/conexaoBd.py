7615c82da6195d879c8642f708483fa9238bbc3f
P7qMXFXqzy <139146483+P7qMXFXqzy@users.noreply.github.com>
2024-02-06 20:25:35 -0300

    Update conexaoBd.py

--- otherArchives/conexaoBd.py
+++ otherArchives/conexaoBd.py
@@ -1,11 +1,23 @@
-from mysql.connector import connect as conectarComBd
-class Conexao:
+from mysql.connector import connect as connectToDb
+class ConnectionClass:
 
-    conexao = conectarComBd(
+    connection = connectToDb(
       host="localhost",
       user="root",
       password="abcd",
       database="gerenciadormercado"
     )
-    cursor = conexao.cursor
-
+    cursor = connection.cursor()
+    #function that will check if a product with the given id exists, will return false if not.
+    def checkIfIdExists(self, insertedId):
+      self.cursor.execute("SELECT idProduto FROM produtos WHERE idProduto = \"" + str(insertedId) + "\"")
+      foundProduct = self.cursor.fetchone()
+      if foundProduct == None: return False
+      else:return True
+    #if the id has been found, request for all of it's data and return it
+    def searchById(self, insertedId):
+      if(self.checkIfIdExists(insertedId) == True):
+        self.cursor.execute("SELECT * FROM produtos WHERE idProduto = \"" + str(insertedId) + "\"")
+        foundProduct = self.cursor.fetchone()
+        return foundProduct
+      else:return None
