14e1048f1ab71bc8ba86bc76b3d5a276c6b72e46
P7qMXFXqzy <139146483+P7qMXFXqzy@users.noreply.github.com>
2024-02-08 13:43:17 -0300

    Update registerScreen.py

--- otherArchives/registerScreen.py
+++ otherArchives/registerScreen.py
@@ -11,17 +11,21 @@ class Window (QMainWindow):
     idText = ""
     dbObject = ConnectionClass()
     receiptText = ""
+    productsNames = []
+    productsPrices = []
+    totalPrice = 0.00
     def __init__(self):
         super(Window, self).__init__()
         self.setStyleSheet("QMainWindow{background-color:rgb(0,0,100)} QScrollBar{background-color: green; border-radius: 10px; color:yellow}")
         #section of the screen that will show all the products bought (also works as a preview of the receipt), will be updated after every new product inserted
+        global scannedProducts
         scannedProducts = QTextEdit(self)
         scannedProducts.verticalScrollBar().setEnabled(True)
         scannedProducts.horizontalScrollBar().setEnabled(False)
         scannedProducts.setGeometry(0,0,floor((monitorSizes[0]/1.7)),floor(monitorSizes[1]/1.2))
         scannedProducts.setText(self.receiptText)
         scannedProducts.setReadOnly(True)
-        scannedProducts.setStyleSheet("QTextEdit{background-color:rgb(100,100,100); color:yellow; font-size:15px; line-height:50px}")
+        scannedProducts.setStyleSheet("QTextEdit{background-color:rgb(100,100,100); color:yellow; font-size:20px; line-height:50px}")
         #inserted code of the product
         global insertedId
         insertedId = QLineEdit(self)
@@ -31,9 +35,10 @@ class Window (QMainWindow):
         insertedId.setText(self.idText)
         insertedId.setReadOnly(True)
         #label that will show the total price of the purchase, will be updated after every new product inserted
+        global totalPriceLabel
         totalPriceLabel = QLabel(self)
         totalPriceLabel.setGeometry(10, floor((monitorSizes[1]/1.2)), (scannedProducts.width() - 10), 80)
-        totalPriceLabel.setText("Total price: R$0,00")
+        totalPriceLabel.setText(str("Total Price: R$0,00"))
         totalPriceLabel.setStyleSheet("QLabel{color: yellow; font-size:60px; font-family:'Franklin Gothic Medium'}")
         #button that will finish the purchase and generate the receipt as a image
         finishButton = QPushButton(self)
@@ -57,33 +62,44 @@ class Window (QMainWindow):
         if(len(self.idText) == 4):self.idText = self.idText[1:]
         self.idText = self.idText + newNumber
         insertedId.setText(self.idText)
-    
     #search for the product on the database
     def searchForItem(self):
         requestedProduct = self.dbObject.searchById(self.idText)
-        if(requestedProduct != None): print(requestedProduct)
-    
+        if(requestedProduct != None): 
+            self.productsNames.append(requestedProduct[1])
+            self.productsPrices.append(requestedProduct[2])
+            self.receiptText = self.receiptText + "\n" + str(self.productsNames[-1]) + " - R$" + str(self.productsPrices[-1])
+            self.totalPrice = self.totalPrice + float(self.productsPrices[-1])
+            scannedProducts.setText(self.receiptText)
+            totalPriceLabel.setText("Total Price: R$" + str(self.totalPrice).replace(".", ","))
+    #erase last character in string
+    def eraseLast(self):
+        self.idText = self.idText[:len(self.idText)-1]
+        insertedId.setText(self.idText)
+    #reset everything for the next purchase
+    def reset(self):
+        self.idText = ""
+        self.receiptText = ""
+        self.productsNames = []
+        self.productsPrices = []
+        self.totalPrice = 0.00
+        insertedId.setText(self.idText)
+        scannedProducts.setText(self.receiptText)
+        totalPriceLabel.setText("Total Price: R$0,00")
     #keyboard actions
     def keyPressEvent(self, event):
-        if event.key() == Qt.Key_1:
-            self.updateId("1")
-        elif event.key() == Qt.Key_2:
-            self.updateId("2")
-        elif event.key() == Qt.Key_3:
-            self.updateId("3")
-        elif event.key() == Qt.Key_4:
-            self.updateId("4")
-        elif event.key() == Qt.Key_5:
-            self.updateId("5")
-        elif event.key() == Qt.Key_6:
-            self.updateId("6")
-        elif event.key() == Qt.Key_7:
-            self.updateId("7")
-        elif event.key() == Qt.Key_8:
-            self.updateId("8")
-        elif event.key() == Qt.Key_9:
-            self.updateId("9")
-        elif event.key() == Qt.Key_0:
-            self.updateId("0")
-        elif event.key() == Qt.Key_Return:
-            self.searchForItem()
+        if event.key() == Qt.Key_1: self.updateId("1")
+        elif event.key() == Qt.Key_2: self.updateId("2")
+        elif event.key() == Qt.Key_3: self.updateId("3")
+        elif event.key() == Qt.Key_4: self.updateId("4")
+        elif event.key() == Qt.Key_5: self.updateId("5")
+        elif event.key() == Qt.Key_6: self.updateId("6")
+        elif event.key() == Qt.Key_7: self.updateId("7")
+        elif event.key() == Qt.Key_8: self.updateId("8")
+        elif event.key() == Qt.Key_9: self.updateId("9")
+        elif event.key() == Qt.Key_0: self.updateId("0")
+        elif event.key() == Qt.Key_Return: self.searchForItem()
+        elif event.key() == Qt.Key_Backspace: self.eraseLast()
+        elif event.key() == Qt.Key_End:
+            print('s')
+        elif event.key() == Qt.Key_R: self.reset()
